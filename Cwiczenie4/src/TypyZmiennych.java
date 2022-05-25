5d0a10b7602cc2a901e250dff283421cdd2727af
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-05-25 22:41:06 +0200

    Dodanie typów zmiennych

---
+++ Cwiczenie4/src/TypyZmiennych.java
@@ -0,0 +1,60 @@
+public class TypyZmiennych {
+    public static void main(String[] args) {
+        System.out.println("Lekcja o typach zmiennych");
+
+        byte zmiennaByte;
+        System.out.println("Zmienna byte");
+        System.out.println("Ilosc bitow: " + Byte.SIZE);
+        System.out.println("Ilosc bajtow: " + Byte.BYTES);
+        System.out.println("Wartosc maksymalna: " + Byte.MAX_VALUE);
+        System.out.println("Wartosc minimalna: " + Byte.MIN_VALUE);
+        System.out.println();
+
+        short zmiennaShort;
+        System.out.println("Zmienna short");
+        System.out.println("Ilosc bitow: " + Short.SIZE);
+        System.out.println("Ilosc bajtow: " + Short.BYTES);
+        System.out.println("Wartosc maksymalna: " + Short.MAX_VALUE);
+        System.out.println("Wartosc minimalna: " + Short.MIN_VALUE);
+        System.out.println();
+
+        int zmiennaInteger;
+        System.out.println("Zmienna integer");
+        System.out.println("Ilosc bitow: " + Integer.SIZE);
+        System.out.println("Ilosc bajtow: " + Integer.BYTES);
+        System.out.println("Wartosc maksymalna: " + Integer.MAX_VALUE);
+        System.out.println("Wartosc minimalna: " + Integer.MIN_VALUE);
+        System.out.println();
+
+        long zmiennaLong;
+        System.out.println("Zmienna long");
+        System.out.println("Ilosc bitow: " + Long.SIZE);
+        System.out.println("Ilosc bajtow: " + Long.BYTES);
+        System.out.println("Wartosc maksymalna: " + Long.MAX_VALUE);
+        System.out.println("Wartosc minimalna: " + Long.MIN_VALUE);
+        System.out.println();
+
+        float zmiennaFloat;
+        System.out.println("Zmienna float");
+        System.out.println("Ilosc bitow: " + Float.SIZE);
+        System.out.println("Ilosc bajtow: " + Float.BYTES);
+        System.out.println("Wartosc maksymalna: " + Float.MAX_VALUE);
+        System.out.println("Wartosc minimalna: " + Float.MIN_VALUE);
+        System.out.println();
+
+        double zmiennaDouble;
+        System.out.println("Zmienna double");
+        System.out.println("Ilosc bitow: " + Double.SIZE);
+        System.out.println("Ilosc bajtow: " + Double.BYTES);
+        System.out.println("Wartosc maksymalna: " + Double.MAX_VALUE);
+        System.out.println("Wartosc minimalna: " + Double.MIN_VALUE);
+        System.out.println();
+
+        boolean zmiennaBoolean;
+        System.out.println("Zmienna boolean");
+        System.out.println("Prawda: " + Boolean.TRUE);
+        System.out.println("Falsz: " + Boolean.FALSE);
+        System.out.println();
+
+    }
+}
