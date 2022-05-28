2270a875e9e3ca75890c41364275b1ea89828f0c
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-05-28 15:43:18 +0200

    import statyczny

--- ProgramowanieObiektowe/src/pl/am/oop/statyczne/Statyczne.java
+++ ProgramowanieObiektowe/src/pl/am/oop/statyczne/Statyczne.java
@@ -2,9 +2,14 @@ package pl.am.oop.statyczne;
 
 import java.util.Random;
 
+//import statyczny:
+import static java.lang.Math.PI;
+import static java.lang.Math.max;
+
 public class Statyczne {
     public static void main(String[] args) {
         Random random = new Random();
-        System.out.println(Math.max(1, 5));
+        System.out.println(max(1, 5));
+        System.out.println(PI);
     }
 }
