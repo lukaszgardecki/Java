a36bca6fb2319ccbe67fde6694a2e3967f3012fe
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-05-28 18:15:14 +0200

    c.d

---
+++ ProgramowanieObiektowe/src/pl/am/oop/dziedziczenie/Dziedziczenie.java
@@ -0,0 +1,22 @@
+package pl.am.oop.dziedziczenie;
+
+public class Dziedziczenie {
+    public static void main(String[] args) {
+
+        Object object = new Object();
+        System.out.println(object.toString());
+        System.out.println(object);
+        System.out.println(object.hashCode());
+        System.out.println(object.getClass().getName());
+        System.out.println(object.equals(object));
+
+        Prostokat prostokat = new Prostokat(2, 5);
+        Kwadrat kwadrat = new Kwadrat(7);
+
+        kwadrat.
+
+        System.out.println(prostokat);
+        System.out.println(kwadrat);
+
+    }
+}
