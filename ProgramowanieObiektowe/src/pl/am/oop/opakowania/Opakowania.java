9314d1685d130d6c68d020568b15e4b8d0a7f969
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-05-28 22:44:57 +0200

    opakowania

---
+++ ProgramowanieObiektowe/src/pl/am/oop/opakowania/Opakowania.java
@@ -0,0 +1,29 @@
+package pl.am.oop.opakowania;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Opakowania {
+    public static void main(String[] args) {
+        Byte zmiennaByte;
+        Short zmiennaShort;
+        Integer zmiennaInt;
+        Long zmiennaLong;
+        Float zmiennaFloat;
+        Double zmiennaDouble;
+        Boolean zmiennaBoolean;
+        Character zmiennaChar;
+
+        zmiennaInt = 34;
+
+        ArrayList<Integer> liczby = new ArrayList<>();
+        int[] tablica;
+
+        liczby.add(6);
+
+        int prostyInt = zmiennaInt;
+
+        int kolejnyInt = Integer.parseInt("6");
+
+    }
+}
