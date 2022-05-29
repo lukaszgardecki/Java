5ed9c0dcf7056129b5d2ccd6d2b575eb93154708
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-05-29 20:59:27 +0200

    Dodanie okna dialogowego OProgramie

--- Java_GUI/src/pl/am/projects/Menu/JMenuTest2.java
+++ Java_GUI/src/pl/am/projects/Menu/JMenuTest2.java
@@ -29,32 +29,35 @@ public class JMenuTest2 extends JFrame implements ActionListener {
             menuPlik.add(mZapisz);
             menuPlik.addSeparator();
             mWyjscie = new JMenuItem("Wyjœcie");
-            menuPlik.add(mWyjscie);
             mWyjscie.addActionListener(this);
+            menuPlik.add(mWyjscie);
             //mo¿na wywo³aæ mWyjœcie jakimœ skrótem klawiszowym:
             mWyjscie.setAccelerator(KeyStroke.getKeyStroke("ctrl X"));
 
 
         menuNarzedzia = new JMenu("Narzêdzia");
         menuBar.add(menuNarzedzia);
-            mNarz1 = new JMenuItem("Narz1");
+            mNarz1 = new JMenuItem("Narzêdzia 1");
             //Mo¿emy wy³¹czyæ dan¹ pozycjê
             //mNarz1.setEnabled(false);
             menuNarzedzia.add(mNarz1);
-            mNarz2 = new JMenuItem("Narz2");
+            mNarz2 = new JMenuItem("Narzêdzia 2");
             menuNarzedzia.add(mNarz2);
             menuOpcje = new JMenu("Opcje");
             menuNarzedzia.add(menuOpcje);
                 mOpcja1 = new JMenuItem("Opcja 1");
                 menuOpcje.add(mOpcja1);
                 chOpcja2 = new JCheckBoxMenuItem ("Opcja 2");
+                chOpcja2.addActionListener(this);
                 menuOpcje.add(chOpcja2);
 
 
         menuPomoc = new JMenu("Pomoc");
         menuBar.add(menuPomoc);
             mOProgramie = new JMenuItem("O programie");
+            mOProgramie.addActionListener(this);
             menuPomoc.add(mOProgramie);
+
     }
 
 
@@ -63,6 +66,16 @@ public class JMenuTest2 extends JFrame implements ActionListener {
         Object z = e.getSource();
         if (z==mWyjscie)
             dispose();
+        if (z == chOpcja2) {
+            if (chOpcja2.isSelected()) {
+                mNarz1.setEnabled(true);
+            } else if (!chOpcja2.isSelected())
+                mNarz1.setEnabled(false);
+        }
+        if (z == mOProgramie) {
+            //wyœwietlenie okna dialogowego
+            JOptionPane.showMessageDialog(this, "Program demonstruje wykorzystanie JMenuBar i JMenu \n Wersja 1.0");
+        }
 
     }
 
