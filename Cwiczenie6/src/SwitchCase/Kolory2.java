5fc8bad69fb6c9e2cd2821df2e0661564a027a17
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-06-03 20:49:27 +0200

    SwitchCase Kolory na dwa sposoby

---
+++ Cwiczenie6/src/SwitchCase/Kolory2.java
@@ -0,0 +1,39 @@
+package SwitchCase;
+
+public class Kolory2 {
+
+    public enum Kolor {
+        CZERWONY,
+        ZIELONY,
+        NIEBIESKI
+    }
+
+
+    public static void main(String[] args) {
+        Kolor kolor = Kolor.CZERWONY;
+
+//      Zapis sposób 1:
+//        switch (kolor) {
+//            case CZERWONY:
+//                System.out.println("Kolor czerwony.");
+//                break;
+//            case NIEBIESKI:
+//                System.out.println("Kolor niebieski.");
+//                break;
+//            case ZIELONY:
+//                System.out.println("Kolor zielony.");
+//                break;
+//            default:
+//                System.out.println("Inny kolor.");
+//                break;
+//        }
+
+        //ten sam zapis, sposób 2, nie trzeba pisaæ breaka:
+        switch (kolor) {
+            case CZERWONY -> System.out.println("Kolor czerwony.");
+            case NIEBIESKI -> System.out.println("Kolor niebieski.");
+            case ZIELONY -> System.out.println("Kolor zielony.");
+            default -> System.out.println("Inny kolor.");
+        }
+    }
+}
