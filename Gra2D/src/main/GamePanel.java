750bf4e3c077acbd7eb1e25cb3d94ff330a1bb3d
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-06-05 23:32:11 +0200

    ciąg dalszy Gry2D. nie działa kod. zatrzymuje się na ścieżkach do grafik .png

--- Gra2D/src/main/GamePanel.java
+++ Gra2D/src/main/GamePanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,7 +11,7 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16; //kafelka 16x16 px
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale; //kafelka 48x48 px
+    public final int tileSize = originalTileSize * scale; //kafelka 48x48 px
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; //768px
@@ -21,6 +23,7 @@ public class GamePanel extends JPanel implements Runnable{
     KeyHandler keyH = new KeyHandler();
     //Thread jest czymœ co mo¿na rozpocz¹æ i zakoñczyæ:
     Thread gameThread;
+    Player player = new Player(this, keyH);
 
 
     //Ustawienie domyœlnej pozycji gracza
@@ -123,26 +126,15 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     public void update() {
-        if (keyH.upPressed == true) {
-            playerY -= playerSpeed;
-        } else if (keyH.downPressed == true) {
-            playerY += playerSpeed;
-        } else if (keyH.leftPressed == true) {
-            playerX -= playerSpeed;
-        } else if (keyH.rightPressed == true) {
-            playerX += playerSpeed;
-        }
+        player.update();
     }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         //rzutowanie! konwertowanie zamiana itp..Zamieniamy zwyk³e Graphics na Graphics2D poniewa¿ ma ona wiêcej fukcji:
         Graphics2D g2 = (Graphics2D)g;
-
-        g2.setColor(Color.white);
-
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
-
+        player.draw(g2);
         g2.dispose();
     }
 }
