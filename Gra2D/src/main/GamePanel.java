d50d4df5f94c2f6221ecf9f744f88879761fa31c
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-06-05 19:54:14 +0200

    Gra2D c.d

--- Gra2D/src/main/GamePanel.java
+++ Gra2D/src/main/GamePanel.java
@@ -15,14 +15,27 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; //768px
     final int screenHeight = tileSize * maxScreenRow; //576px
 
+    //FPS:
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     //Thread jest czymœ co mo¿na rozpocz¹æ i zakoñczyæ:
     Thread gameThread;
+
+
+    //Ustawienie domyœlnej pozycji gracza
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
 
     //Konstruktor
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -30,21 +43,95 @@ public class GamePanel extends JPanel implements Runnable{
         gameThread.start();
     }
 
+    //METODA PIERWSZA: metoda z usypianiem
+//    @Override
+//    public void run() {
+//
+//        double drawInterval = 1000000000/FPS; // 0.01666 sekund
+//        double nextDrawTime = System.nanoTime() + drawInterval;
+//
+//        while (gameThread != null) {
+//            //.nanoTime() jest bardziej dok³adny ni¿ currentTimeMillis()
+//            long currentTime = System.nanoTime(); // j
+//            //long currentTime2 = System.currentTimeMillis();
+//
+//            // 1.Aktualizacja: aktualizuj informacje takie jak pozycja postaci
+//            update();
+//
+//            //2.Rysunek: Rysuj ekran z aktualymi informacjami
+//            repaint();
+//
+//            try {
+//                double remainingTime = nextDrawTime - System.nanoTime();
+//                remainingTime = remainingTime/1000000;
+//
+//                if (remainingTime < 0) {
+//                    remainingTime = 0;
+//                }
+//
+//                Thread.sleep((long) remainingTime);
+//
+//                nextDrawTime += drawInterval;
+//
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//            //gdyby nie diza³a³o to wy¿ej:
+////            try {
+////                Thread.sleep((long) remainingTime);
+////            } catch (InterruptedException e) {
+////                throw new RuntimeException(e);
+////            }
+//        }
+//    }
+
+    //METODA DRUGA: z delt¹
     @Override
     public void run() {
+
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+
         while (gameThread != null) {
-            //System.out.println("The game loop is running");
 
-            // 1.Aktualizacja: aktualizuj informacje takie jak pozycja postaci
-            update();
+            currentTime = System.nanoTime();
 
-            //2.Rysunek: Rysuj ekran z aktualymi informacjami
-            repaint();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if (timer >= 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+
         }
     }
 
     public void update() {
-
+        if (keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        } else if (keyH.downPressed == true) {
+            playerY += playerSpeed;
+        } else if (keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -54,7 +141,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         g2.setColor(Color.white);
 
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
 
         g2.dispose();
     }
