ba937af949887f5dfa0ebc8afc69533b6e41f3fe
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-07-21 23:34:21 +0200

    dodanie koeljnych lekcji

---
+++ Bulls and Cows/Topics/Comparable/Value class/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Age {
+    private final int value;
+
+    public Age(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
+
+// do not change the code below
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Age> list = Arrays.stream(sc.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .mapToObj(Age::new)
+                .sorted()
+                .collect(Collectors.toList());
+
+        Checker.check(list);
+    }
+}
