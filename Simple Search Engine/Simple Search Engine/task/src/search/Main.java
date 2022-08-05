735cf0fdb74c8b79f4333398f090da8d140fe1f3
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-08-06 00:45:43 +0200

    simple search engine c.d

--- Simple Search Engine/Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/Simple Search Engine/task/src/search/Main.java
@@ -1,7 +1,153 @@
 package search;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Main {
+    static ArrayList<String> resultList = new ArrayList<>();
+    static ArrayList<String> peopleList = new ArrayList<>();
+    static SortedMap<String, ArrayList<Integer>> invertedIndexMap = new TreeMap<>();
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        readFile(args);
+        createInvertedIndex();
+        play();
     }
+
+    public static void play() {
+        Scanner scanner = new Scanner(System.in);
+
+//        System.out.println("Enter the number of people:");
+//        int amount = scanner.nextInt();
+//        scanner.nextLine();
+//        peopleTab = new String[amount];
+//
+//        System.out.println("Enter all people:");
+//        for(int i = 0; i < amount; i++) {
+//            peopleTab[i] = scanner.nextLine();
+//        }
+
+        while (true) {
+            displayMenu();
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "0":
+                    System.out.println("\nBye!");
+                    return;
+                case "1":
+                    findPerson();
+                    break;
+                case "2":
+                    printAllPeople();
+                    break;
+                default:
+                    System.out.println("\nIncorrect option! Try again!");
+            }
+        }
+    }
+
+    public static void displayMenu() {
+        System.out.println("\n=== Menu ===");
+        System.out.println("1. Find a person");
+        System.out.println("2. Print all people");
+        System.out.println("0. Exit");
+    }
+
+    public static void findPerson() {
+        Scanner scanner = new Scanner(System.in);
+
+        //System.out.println("\nEnter the number of search queries:");
+        //int howManyQueries = scanner.nextInt();
+        //scanner.nextLine();
+
+        //for (int i = 0; i < howManyQueries; i++) {
+
+            //System.out.println("\nEnter data to search poeple:");
+            System.out.println("\nEnter a name or email to search all suitable people.");
+            String searchFor = scanner.nextLine().toLowerCase();
+
+            for (String el : peopleList) {
+                if (el.toLowerCase().contains(searchFor)) {
+                    resultList.add(el);
+
+                }
+            }
+
+            if (resultList.size() == 0) {
+                System.out.println("No matching people found.");
+            } else {
+                //System.out.println("\nFound people:");
+
+
+                // TUTAJ TRZEBA ZMIENIÆ SEARCH FOR ¯EBY W MAPIE NIE SZUKAÆ PO TYM SO SIÊ WPISZE
+                //DLA ZNAKU @ NPE
+                // JEST KILKA WYNIKÓW NIE JEDEN
+                int amount = invertedIndexMap.get(searchFor).size();
+
+
+
+
+                System.out.printf("\n%d persons found:\n", amount);
+
+                resultList.forEach(System.out::println);
+            }
+            resultList.clear();
+        //}
+    }
+
+    public static void printAllPeople() {
+        System.out.println("\n=== List of people ===");
+        for(String el : peopleList) {
+            System.out.println(el);
+        }
+    }
+
+    public static void readFile(String[] args) {
+        String importPath = "";
+        //int counter = 0;
+
+        for(int i = 0; i < args.length; i += 2) {
+            if (args[i].equals("--data")) {
+                importPath = args[i+1];
+            }
+        }
+
+        File file = new File(importPath);
+        try (Scanner importScanner = new Scanner(file)) {
+            while (importScanner.hasNextLine()) {
+                peopleList.add(importScanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+    }
+
+    public static void createInvertedIndex() {
+        for (int i = 0; i < peopleList.size(); i++) {
+            String[] temp = peopleList.get(i).split("\\s+");
+
+            for (int k = 0; k < temp.length; k++) {
+
+                String el = temp[k].toLowerCase();
+
+                if(invertedIndexMap.containsKey(el)) {
+                    //pojedyncza lista wyst¹pieñ dla konkretnego s³owa
+                    ArrayList<Integer> li = invertedIndexMap.get(el);
+                    li.add(i);
+                    invertedIndexMap.replace(el, li);
+                } else {
+                    ArrayList<Integer> li2 = new ArrayList<>();
+                    li2.add(i);
+                    invertedIndexMap.putIfAbsent(el, li2);
+                }
+            }
+        }
+
+        //System.out.println(invertedIndexMap);
+    }
+
 }
--- Simple Search Engine/Simple Search Engine/task/test/SimpleSearchEngineTest.java
+++ Simple Search Engine/Simple Search Engine/task/test/SimpleSearchEngineTest.java
@@ -1,82 +1,219 @@
-import org.hyperskill.hstest.exception.outcomes.WrongAnswer;
+import org.hyperskill.hstest.testcase.TestCase;
 import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
-import org.hyperskill.hstest.testcase.TestCase;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class TestClue {
-    boolean correctness;
-    int position;
+    String input;
 
-    TestClue(boolean correct, int pos) {
-        correctness = correct;
-        position = pos;
+    TestClue(String input) {
+        this.input = input;
     }
 }
 
 public class SimpleSearchEngineTest extends StageTest<TestClue> {
 
+    public static final String test1 =
+        "2\n" +
+        "0";
+
+    public static final String test2 =
+        "1\n" +
+        "qwerty\n" +
+        "0";
+
+    public static final String test3 =
+        "1\n" +
+        "Leopold\n" +
+        "0";
+
+    public static final String test4 =
+        "3\n" +
+        "1\n" +
+        "Bob\n" +
+        "2\n" +
+        "2\n" +
+        "1\n" +
+        "Leopold\n" +
+        "0";
+
+    public static final String test5 =
+        "1\n" +
+        "@\n" +
+        "1\n" +
+        "Leopold\n" +
+        "0";
+
+    public static final String test6 =
+        "0";
+
+    public static final String test7 =
+        "1\n" +
+        "this text never find some match\n" +
+        "0";
+
+    public static final String test8 =
+        "1\n" +
+        "h\n" +
+        "1\n" +
+        "gallien@evilcorp.com\n" +
+        "0";
+
+    public static final String test9 =
+        "4\n" +
+        "2\n" +
+        "2\n" +
+        "1\n" +
+        "this text never gonna be matched\n" +
+        "1\n" +
+        "h\n" +
+        "1\n" +
+        "gallien@evilcorp.com\n" +
+        "0";
+
     @Override
     public List<TestCase<TestClue>> generate() {
-        return Arrays.asList(
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(true, 2))
-                .setInput("hello my name is alex\nmy"),
 
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(true, 5))
-                .setInput("hello my name is alex\nalex"),
+        List<TestCase<TestClue>> tests = new ArrayList<>();
 
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(true, 1))
-                .setInput("what a beautiful place\nwhat"),
+        for (String input : new String[]{
+            test1, test2, test3, test4, test5, test6, test7, test8, test9}) {
 
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(true, 1))
-                .setInput("hi\nhi"),
+            tests.add(new TestCase<TestClue>()
+                .setAttach(new TestClue(input))
+                .setInput(input)
+                .addArguments("--data", "names.txt")
+                .addFile("names.txt", SearchEngineTests.NAMES));
+        }
 
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(false, 0))
-                .setInput("hi\nhello"),
-
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(false, 0))
-                .setInput("twenty one pilots white stripes queen system of a down\nhello"),
-
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(true, 9))
-                .setInput("twenty one pilots white stripes queen system of a down\na"),
-
-            new TestCase<TestClue>()
-                .setAttach(new TestClue(false, 0))
-                .setInput("one two three\nfour")
-        );
+        return tests;
     }
 
     @Override
     public CheckResult check(String reply, TestClue clue) {
-        reply = reply.trim();
-        if (reply.length() == 0) {
-            throw new WrongAnswer("Output is empty");
-        }
+        String cR = "\n";
+        List<String> outputLines = new LinkedList<String>(Arrays.asList(reply.split(cR)));
+        String[] inputLines = clue.input.split(cR);
+        String[] reference;
+        String[] currentSearchResult;
 
-        String[] lines = reply.split("\n");
-        String lastLine = lines[lines.length - 1].trim().toLowerCase();
+        reference = SearchEngineTests.NAMES.split("\n");
 
-        if (lines.length == 1) {
-            boolean correct = clue.correctness;
-            int index = clue.position;
-            if (correct) {
-                return new CheckResult(lastLine.contains(Integer.toString(index)));
-            } else {
-                return new CheckResult(lastLine.contains("not found"));
+        //clear the list of unnecessary lines, if any
+        List<String> cleanedOutput = new ArrayList<String>();
+        for (int i = 0; i < outputLines.size(); i++) {
+            if (containsItemFromList(outputLines.get(i), reference)) {
+                cleanedOutput.add(outputLines.get(i).toLowerCase());
             }
-        } else {
-            return new CheckResult(false, "You should have responsesFromClient only one line instead of " +
-                lines.length);
         }
+
+        int currentInputLine = 0;
+        int currentOutputLine = 0;
+
+        int actionType = -1;
+
+        while (actionType != 0) {
+            try {
+                actionType = Integer.parseInt(inputLines[currentInputLine]);
+            } catch (NumberFormatException e) {
+                return new CheckResult(false,
+                    "The number of menu item must be number!");
+            }
+
+            switch (actionType) {
+                case 1:
+                    currentInputLine++;
+
+                    String toSearch = inputLines[currentInputLine].trim().toLowerCase();
+
+                    currentInputLine++;
+
+                    List<String> intendedResult = new ArrayList<>();
+
+                    for (String s : reference) {
+                        s = s.toLowerCase();
+                        if (s.contains(" " + toSearch + " ")
+                            || s.startsWith(toSearch + " ")
+                            || s.endsWith(" " + toSearch)) {
+
+                            intendedResult.add(s);
+                        }
+                    }
+
+
+
+                    currentSearchResult = new String[intendedResult.size()];
+                    for (int i = 0; i < intendedResult.size(); i++) {
+                        try {
+                            currentSearchResult[i] = cleanedOutput.get(currentOutputLine++);
+                        } catch (IndexOutOfBoundsException e) {
+                            return new CheckResult(false,
+                                "Seems like you output less than expected. " +
+                                    "Either you've lost someone in the printing of all " +
+                                    "people, or you haven't printed all the necessary " +
+                                    "people in the search.");
+                        }
+                    }
+
+                    String[] correctOutput = intendedResult.toArray(String[]::new);
+
+                    Arrays.sort(correctOutput);
+                    Arrays.sort(currentSearchResult);
+
+                    if (!Arrays.equals(correctOutput, currentSearchResult)) {
+                        return new CheckResult(false,
+                            "Search result is not equal " +
+                                "to the expected search");
+                    }
+                    break;
+                case 2:
+                    currentInputLine++;
+
+                    List<String> intendedResultAll = new ArrayList<>();
+
+                    for (String s : reference) {
+                        s = s.toLowerCase();
+                        intendedResultAll.add(s);
+                    }
+
+                    String[] userResultAll = new String[intendedResultAll.size()];
+                    for (int i = 0; i < intendedResultAll.size(); i++) {
+                        try {
+                            userResultAll[i] = cleanedOutput.get(currentOutputLine++);
+                        } catch (IndexOutOfBoundsException e) {
+                            return new CheckResult(false,
+                                "Seems like you output less than expected. " +
+                                    "Either you've lost someone in the printing of all " +
+                                    "people, or you haven't printed all the necessary " +
+                                    "people in the search.");
+                        }
+                    }
+
+                    String[] correctOutputAll = intendedResultAll.toArray(String[]::new);
+
+                    Arrays.sort(correctOutputAll);
+                    Arrays.sort(userResultAll);
+
+                    if (!Arrays.equals(correctOutputAll, userResultAll)) {
+                        return new CheckResult(false,
+                            "Looks like you're printing " +
+                                "unknown people when you enter option 2.");
+                    }
+                    break;
+                case 0:
+                    return CheckResult.correct();
+                default:
+                    currentInputLine++;
+                    break;
+            }
+        }
+
+        return CheckResult.correct();
+    }
+
+    private static boolean containsItemFromList(String inputStr, String[] items) {
+        return Arrays.stream(items).parallel().anyMatch(inputStr::contains);
     }
 }
 
