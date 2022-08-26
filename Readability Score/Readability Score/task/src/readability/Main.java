7259830f72a46dde1c0fc6c1c4d39895d0b796eb
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-08-26 23:25:51 +0200

    skończenie projektu readability score

--- Readability Score/Readability Score/task/src/readability/Main.java
+++ Readability Score/Readability Score/task/src/readability/Main.java
@@ -13,16 +13,10 @@ public class Main {
     private static double polysyllables = 0;
     private static final Scanner scanner = new Scanner(System.in);
     private static ArrayList<String> textList = new ArrayList<>();
-    private static String path = "F:\\1. S  T  U  D  I  A\\Æwiczenia z programowania\\Java\\Readability Score\\Readability Score\\task\\src\\readability\\text.txt";
-    //private static String path = args[0];
-
-
-
-
-
-
+    //private static String path = "F:\\1. S  T  U  D  I  A\\Æwiczenia z programowania\\Java\\Readability Score\\Readability Score\\task\\src\\readability\\text.txt";
 
     public static void main(String[] args) {
+        String path = args[0];
         readTextFile(path);
 
         System.out.println("The text is:");
@@ -39,12 +33,6 @@ public class Main {
 
         printScores(scoreType);
     }
-
-
-
-
-
-
     public static void readTextFile(String path) {
         File input = new File(path);
 
@@ -52,14 +40,34 @@ public class Main {
             while (inputSc.hasNextLine()) {
                 String line = inputSc.nextLine();
                 textList.add(line);
-                String[] sentTab = line.split("[.!?]\\s+");
+                String[] sentTab = line.split("[.!?]+\\s");
                 characters += sentTab.length-1;
                 sentences += sentTab.length;
                 for (String s : sentTab) {
                     String[] wordsTab = s.split("\\s+");
                     words += wordsTab.length;
                     for (String w : wordsTab) {
+                        int vovel = 0;
                         characters += w.length();
+
+                        /* ----- find amount of syllables ----- */
+                        w = w.replaceAll("e\\b", "")
+                                .replaceAll("[Yy]ou", "a")
+                                .replaceAll("[aeiouyAEIOUY]{2}", "a")
+                                .replaceAll(" th "," a ")
+                                .replaceAll(",","")
+                                .replaceAll(" w "," a ")
+                                .replaceAll("\\d+", "a")
+                                .replaceAll("[^aeiouyAEIOUY]", "");
+
+                        for (int k = 0; k < w.length(); k++) {
+                            String c = Character.toString(w.charAt(k)).toLowerCase();
+                            if (c.matches("[aeiouy]")) vovel++;
+                        }
+
+                        if (vovel == 0) syllables++;
+                        if (vovel > 2) polysyllables++;
+                        syllables += vovel;
                     }
                 }
             }
@@ -67,10 +75,11 @@ public class Main {
             System.out.println("File not found.");
         }
     }
-
     public static String getAge(int score) {
         String age = "";
         switch (score) {
+            case 0:
+                age = "5"; break;
             case 1:
                 age = "6"; break;
             case 2:
@@ -97,7 +106,7 @@ public class Main {
                 age = "17"; break;
             case 13:
                 age = "18"; break;
-            case 14:
+            default:
                 age = "22"; break;
         }
         return age;
@@ -116,42 +125,48 @@ public class Main {
         double S = sentences / words * 100;
         return 0.0588 * L - 0.296 * S - 15.8;
     }
-    public static double countAverage() {
-
-    }
     public static void printScores(String scoreType) {
+        String ariAge = getAge((int) Math.ceil(getARI()));
+        String fkAge = getAge((int) Math.ceil(getFleschKincaid()));
+        String smogAge = getAge((int) Math.ceil(getSmog()));
+        String clAge = getAge((int) Math.ceil(getColemanLiau()));
+        int a = Integer.parseInt(ariAge);
+        int f = Integer.parseInt(fkAge);
+        int s = Integer.parseInt(smogAge);
+        int c = Integer.parseInt(clAge);
+        double average = (double) (a + f + s + c) / 4;
+        String av = String.format("%.2f", average);
+
         System.out.println();
         switch (scoreType) {
             case "ari":
                 System.out.printf("Automated Readability Index: %.2f (about %s-year-olds).\n",
-                        getARI(), getAge((int) Math.ceil(getARI())));
+                        getARI(), ariAge);
                 break;
             case "fk":
                 System.out.printf("Flesch–Kincaid readability tests: %.2f (about %s-year-olds).\n",
-                        getFleschKincaid(), getAge((int) Math.ceil(getARI())));
+                        getFleschKincaid(), fkAge);
                 break;
             case "smog":
                 System.out.printf("Simple Measure of Gobbledygook: %.2f (about %s-year-olds).\n",
-                        getSmog(), getAge((int) Math.ceil(getARI())));
+                        getSmog(), smogAge);
                 break;
             case "cl":
                 System.out.printf("Coleman–Liau index: %.2f (about %s-year-olds).\n", getColemanLiau(),
-                        getAge((int) Math.ceil(getARI())));
+                        clAge);
                 break;
             case "all":
                 System.out.printf("Automated Readability Index: %.2f (about %s-year-olds).\n",
-                        getARI(), getAge((int) Math.ceil(getARI())));
+                        getARI(), ariAge);
                 System.out.printf("Flesch–Kincaid readability tests: %.2f (about %s-year-olds).\n",
-                        getFleschKincaid(), getAge((int) Math.ceil(getARI())));
+                        getFleschKincaid(), fkAge);
                 System.out.printf("Simple Measure of Gobbledygook: %.2f (about %s-year-olds).\n",
-                        getSmog(), getAge((int) Math.ceil(getARI())));
+                        getSmog(), smogAge);
                 System.out.printf("Coleman–Liau index: %.2f (about %s-year-olds).\n",
-                        getColemanLiau(), getAge((int) Math.ceil(getARI())));
+                        getColemanLiau(), clAge);
                 break;
         }
         System.out.println();
-
-        // System.out.printf("The score is: %.2f\n", score);
-        System.out.printf("This text should be understood in average by %s-year-olds", age);
+        System.out.printf("This text should be understood in average by %s-year-olds", av);
     }
 }
