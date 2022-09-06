1ecc74265b4cfa0b752770773348200a71682152
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-09-06 20:36:24 +0200

    spring. ćwiczenie 3

---
+++ _SPRING_/RESTService/src/test/java/com/example/RESTService/RestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RESTService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
