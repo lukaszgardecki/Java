598865669f5282781bbfb2e71279a7682996303e
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-09-09 11:53:57 +0200

    spring security LDAP

---
+++ _SPRING_/Spring_Security_LDAP/src/test/java/com/example/Spring_Security_LDAP/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Security_LDAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
