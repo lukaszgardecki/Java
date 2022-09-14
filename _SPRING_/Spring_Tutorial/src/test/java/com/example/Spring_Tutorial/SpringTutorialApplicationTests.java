d7f9b4b247ea0599f7e009101f69c6ae9a2a8a59
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-09-14 21:58:11 +0200

    spring boot tutorial

---
+++ _SPRING_/Spring_Tutorial/src/test/java/com/example/Spring_Tutorial/SpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
