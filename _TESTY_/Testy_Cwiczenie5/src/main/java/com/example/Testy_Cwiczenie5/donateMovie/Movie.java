af88a1b6ae0645fc693ac6f44b3b38f94ad2de00
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-10-11 19:02:22 +0200

    ćwiczenie testy jednostkowe

---
+++ _TESTY_/Testy_Cwiczenie5/src/main/java/com/example/Testy_Cwiczenie5/donateMovie/Movie.java
@@ -0,0 +1,14 @@
+package com.example.Testy_Cwiczenie5.donateMovie;
+
+public class Movie {
+
+    private int copies;
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void addCopy() {
+        copies++;
+    }
+}
---
+++ _TESTY_/Testy_Cwiczenie5/src/test/java/com/example/Testy_Cwiczenie5/donateMovieTests/DonateMovieTest.java
@@ -0,0 +1,32 @@
+package com.example.Testy_Cwiczenie5.donateMovieTests;
+
+import com.example.Testy_Cwiczenie5.donateMovie.Library;
+import com.example.Testy_Cwiczenie5.donateMovie.Movie;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DonateMovieTest {
+    private final Library library;
+    private final Movie movie;
+
+    public DonateMovieTest() {
+        library = new Library();
+        movie = new Movie();
+    }
+
+    @Test
+    @DisplayName("Check a library has a movie")
+    public void movieAddedToCatalogue() {
+        library.donate(movie);
+        assertTrue(library.contains(movie));
+    }
+
+    @Test
+    public void rentalCopyAdded() {
+        library.donate(movie);
+        assertEquals(1, movie.getCopies());
+    }
+}
