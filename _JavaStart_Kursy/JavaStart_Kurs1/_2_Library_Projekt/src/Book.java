930f1e570c63152181549b36cba9bded925efb20
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-10-28 21:56:40 +0200

    Aktualizacja projektu księgarni

--- _JavaStart_Kursy/JavaStart_Kurs1/_2_Library_Projekt/src/Book.java
+++ _JavaStart_Kursy/JavaStart_Kurs1/_2_Library_Projekt/src/Book.java
@@ -1,8 +1,22 @@
-public class Book {
+class Book {
     String title;
     String author;
     int releaseDate;
     int pages;
     String publisher;
     String isbn;
+
+    public Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.releaseDate = releaseDate;
+        this.pages = pages;
+        this.publisher = publisher;
+        this.isbn = isbn;
+    }
+
+
+    void printInfo() {
+        System.out.printf("%s; %s; %s; %s; %s; %s\n", title, author, releaseDate, pages, publisher, isbn);
+    }
 }
