e8433754e10404847dd2aff640026f3c994bbebd
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-10-29 12:14:32 +0200

    JavaStart aktualizacja projektu biblioteki

--- _JavaStart_Kursy/JavaStart_Kurs1/_2_Library_Projekt/src/pl/javastart/library/app/Library.java
+++ _JavaStart_Kursy/JavaStart_Kurs1/_2_Library_Projekt/src/pl/javastart/library/app/Library.java
@@ -1,6 +1,10 @@
+package pl.javastart.library.app;
+
+import pl.javastart.library.model.Book;
+
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.4";
+        final String appName = "Biblioteka v0.5";
 
         Book book1 = new Book(
                 "W pustyni i w puszczy",
