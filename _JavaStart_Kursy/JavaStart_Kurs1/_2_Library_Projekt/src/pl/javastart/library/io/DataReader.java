3d70ab4bac33a1c04f5017ab911a100020075d98
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-10-30 11:05:57 +0100

    aktualizacja projektu biblioteki

---
+++ _JavaStart_Kursy/JavaStart_Kurs1/_2_Library_Projekt/src/pl/javastart/library/io/DataReader.java
@@ -0,0 +1,32 @@
+package pl.javastart.library.io;
+
+import pl.javastart.library.model.Book;
+
+import java.util.Scanner;
+
+public class DataReader {
+    private Scanner scanner = new Scanner(System.in);
+
+    public Book readAndCreateBook() {
+        System.out.println("Tytu³:");
+        String title = scanner.nextLine();
+        System.out.println("Autor:");
+        String author = scanner.nextLine();
+        System.out.println("Wydawnictwo:");
+        String publisher = scanner.nextLine();
+        System.out.println("ISBN:");
+        String isbn = scanner.nextLine();
+        System.out.println("Rok wydania:");
+        int releaseDate = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Liczba stron:");
+        int pages = scanner.nextInt();
+        scanner.nextLine();
+
+        return new Book(title, author, releaseDate, pages, publisher, isbn);
+    }
+
+    public void close() {
+        scanner.close();
+    }
+}
