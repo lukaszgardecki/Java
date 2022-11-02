0af637734fddfabf4d9a2da2f1e34ae88e4cf0db
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-11-02 13:42:17 +0100

    projekt biblioteki. przeniesienie klasy enum z osobnego pliku do LibraryController jako klasy wewnętrznej

--- _JavaStart_Kursy/JavaStart_Kurs1/_2_Library_Projekt/src/pl/javastart/library/app/LibraryApp.java
+++ _JavaStart_Kursy/JavaStart_Kurs1/_2_Library_Projekt/src/pl/javastart/library/app/LibraryApp.java
@@ -1,7 +1,7 @@
 package pl.javastart.library.app;
 
 class LibraryApp {
-    final static String APP_NAME = "Biblioteka v1.5";
+    final static String APP_NAME = "Biblioteka v1.6";
 
     public static void main(String[] args) {
         System.out.println(APP_NAME);
