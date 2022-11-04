6843718300eba12e727d625eddb23e90ab19d5e4
Łukasz Gardecki <lukas.g1804@gmail.com>
2022-11-04 21:56:30 +0100

    aktualizacja projektu biblioteki

--- _JavaStart_Kursy/JavaStart_Kurs1/_2_Library_Projekt/src/pl/javastart/library/app/LibraryControl.java
+++ _JavaStart_Kursy/JavaStart_Kurs1/_2_Library_Projekt/src/pl/javastart/library/app/LibraryControl.java
@@ -6,7 +6,9 @@ import pl.javastart.library.io.DataReader;
 import pl.javastart.library.io.file.FileManager;
 import pl.javastart.library.io.file.FileManagerBuilder;
 import pl.javastart.library.model.*;
+import pl.javastart.library.model.comparator.AlphabeticalTitleComparator;
 
+import java.util.Comparator;
 import java.util.InputMismatchException;
 
 class LibraryControl {
@@ -50,7 +52,12 @@ class LibraryControl {
     }
 
     private void printUsers() {
-        printer.printUsers(library.getUsers().values());
+        printer.printUsers(library.getSortedUsers(new Comparator<LibraryUser>() {
+            @Override
+            public int compare(LibraryUser p1, LibraryUser p2) {
+                return p1.getLastName().compareToIgnoreCase(p2.getLastName());
+            }
+        }));
     }
 
     private void addUser() {
@@ -80,7 +87,7 @@ class LibraryControl {
     }
 
     private void printMagazines() {
-        printer.printMagazines(library.getPublications().values());
+        printer.printMagazines(library.getSortedPublications(new AlphabeticalTitleComparator()));
     }
 
 
@@ -121,7 +128,7 @@ class LibraryControl {
     }
 
     private void printBooks() {
-        printer.printBooks(library.getPublications().values());
+        printer.printBooks(library.getSortedPublications(new AlphabeticalTitleComparator()));
     }
 
     private void addBook() {
