e4d832e14ba1c7e1b2673c1fe7ae090f43139baa
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-01-27 20:24:09 +0100

    JavaStart, kurs Spring powtórka, sesje

---
+++ _JavaStart_Kursy/JavaStart_Spring/Zad_29_Sesje/src/main/java/com/example/sessions/Zad29SesjeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sessions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zad29SesjeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zad29SesjeApplication.class, args);
+    }
+
+}
---
+++ _JavaStart_Kursy/JavaStart_Spring/Zad_29_Sesje/src/test/java/com/example/sessions/Zad29SesjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sessions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zad29SesjeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
