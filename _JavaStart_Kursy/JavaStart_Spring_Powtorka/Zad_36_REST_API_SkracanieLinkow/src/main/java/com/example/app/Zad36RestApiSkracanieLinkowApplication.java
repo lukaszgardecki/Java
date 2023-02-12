7adbd674416926d231eb7e57bae0b29fdf19ebe3
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-02-12 15:35:26 +0100

    Create new project Zad_36_REST_API_SkracanieLinkow

---
+++ _JavaStart_Kursy/JavaStart_Spring_Powtorka/Zad_36_REST_API_SkracanieLinkow/src/main/java/com/example/app/Zad36RestApiSkracanieLinkowApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zad36RestApiSkracanieLinkowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zad36RestApiSkracanieLinkowApplication.class, args);
+    }
+
+}
