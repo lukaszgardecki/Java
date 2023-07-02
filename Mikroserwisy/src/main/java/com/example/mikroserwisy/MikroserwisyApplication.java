ce4adcf25865f2a6e5bc3e4a526e962bbbb1f84b
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-07-02 14:41:50 +0200

    Microservices initial commit

---
+++ Mikroserwisy/src/main/java/com/example/mikroserwisy/MikroserwisyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mikroserwisy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikroserwisyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MikroserwisyApplication.class, args);
+    }
+
+}
---
+++ Mikroserwisy/src/test/java/com/example/mikroserwisy/MikroserwisyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mikroserwisy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikroserwisyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
