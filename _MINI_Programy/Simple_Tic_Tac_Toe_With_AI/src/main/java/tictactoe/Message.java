673f1a69228f08e60a96be971282629204dbc47d
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-08-03 15:10:56 +0200

    Zmiana struktury katalogu 2

---
+++ _MINI_Programy/Simple_Tic_Tac_Toe_With_AI/src/main/java/tictactoe/Message.java
@@ -0,0 +1,19 @@
+package tictactoe;//package tictactoe;
+//
+//public class Message {
+//    public static void display(int number) {
+//        switch (number) {
+//            case 1: System.out.print("Enter the coordinates: "); break;
+//            case 2: System.out.println("You should enter numbers!"); break;
+//            case 3: System.out.println("Coordinates should be from 1 to 3!"); break;
+//            case 4: System.out.println("This cell is occupied! Choose another one!"); break;
+//            case 5: System.out.println("Draw"); break;
+//            case 6: System.out.println("Game not finished"); break;
+//            case 7: System.out.printf("%s wins\n", Board.getCurrentPlayer()); break;
+//            case 8: System.out.println("Making move level \"easy\""); break;
+//            case 9: System.out.print("\nInput command: "); break;
+//            case 10: System.out.println("Bad parameters!"); break;
+//            case 11: System.out.println("Making move level \"medium\""); break;
+//        }
+//    }
+//}
