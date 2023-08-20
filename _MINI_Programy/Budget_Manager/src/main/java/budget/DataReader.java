445912c3e65bf4553af66ff916c6216429fbfdf4
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-08-20 21:40:18 +0200

    Budget Manager - move loadFile() to DataReader

--- _MINI_Programy/Budget_Manager/src/main/java/budget/DataReader.java
+++ _MINI_Programy/Budget_Manager/src/main/java/budget/DataReader.java
@@ -1,6 +1,7 @@
 package budget;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
@@ -91,6 +92,97 @@ public class DataReader {
 
             System.out.println("\nPurchases were saved!");
         } catch (IOException e) {
+            System.out.println("File not found!");
+        }
+    }
+
+    public void loadFile() {
+        File file = new File(filePath);
+        try (Scanner importScanner = new Scanner(file)) {
+
+            //ustawienie stanu konta:
+            String[] temp = importScanner.nextLine().split("\\$");
+
+            if (temp[1].contains(",")) {
+                balance = Double.parseDouble(temp[1].replace(",", "."));
+            } else {
+                balance = Double.parseDouble(temp[1]);
+            }
+
+            importScanner.nextLine();
+
+            while(importScanner.hasNextLine()) {
+                //linijka z numerem i nazw�
+                String[] temp1 = importScanner.nextLine().split("\\s+");
+                temp1[1] = temp1[1].replace(":", "");
+                int numOfCat = Integer.parseInt(temp1[0]);
+                double price;
+                String name;
+                double sum;
+
+                String firstLine = importScanner.nextLine();
+
+                //je�eli dana kategoria jest pusta to id� do nast�pnej
+                if (!firstLine.equals("\tThe purchase list is empty!")) {
+                    //p�tla do stworzenia listy
+                    while (true) {
+                        if (firstLine.startsWith("\t-")) {
+                            //ostatnie wyst�pienie $ oznacza pocz�tek ceny:
+                            int indexLast$ = firstLine.lastIndexOf("$");
+
+                            String reallyPrice = firstLine.substring(indexLast$ + 1);
+                            String reallyName = firstLine.substring(0, indexLast$);
+
+                            //ca�a linijka podzielona na cz�� nazwy i cz�� ceny
+                            //String[] temp3 = firstLine.split("\\$");
+
+                            //pozbawinie nazwy zb�dnych spacji
+                            String[] nameWithoutSpaces = reallyName.trim().split("\\s+");
+
+                            //cena pojedynczego produktu
+                            if (reallyPrice.contains(",")) {
+                                price = Double.parseDouble(reallyPrice.replace(",", "."));
+                            } else {
+                                price = Double.parseDouble(reallyPrice);
+                            }
+
+                            StringBuilder nameSB = new StringBuilder();
+                            for (String s : nameWithoutSpaces) {
+                                nameSB.append(String.format("%s ", s));
+                            }
+
+                            //nowa linijka usuni�cie pocz�tkowych my�lnik�w i dodatkowej spacji:
+                            nameSB.deleteCharAt(0);
+                            nameSB.deleteCharAt(0);
+
+                            //nowa ca�a linijka (bez zb�dnych tab�w i spacji)
+                            nameSB.append(String.format("$%.2f", price));
+                            name = nameSB.toString();
+
+                            //aktualizacja pierwszej linijki:
+                            firstLine = importScanner.nextLine();
+
+                            //dodaj pozycj� do listy
+                            map.get(numOfCat).getBoughtProducts().add(name);
+                        } else {
+                            break;
+                        }
+                    }
+                    String[] temp5 = importScanner.nextLine().split("\\$");
+                    sum = Double.parseDouble(temp5[1].replace(",", "."));
+
+                    //ustaw sum� kategorii
+                    map.get(numOfCat).setSum(sum);
+
+                    importScanner.nextLine();
+                } else {
+                    importScanner.nextLine();
+                    importScanner.nextLine();
+                    importScanner.nextLine();
+                }
+            }
+            System.out.println("\nPurchases were loaded!");
+        } catch (FileNotFoundException e) {
             System.out.println("File not found!");
         }
     }
