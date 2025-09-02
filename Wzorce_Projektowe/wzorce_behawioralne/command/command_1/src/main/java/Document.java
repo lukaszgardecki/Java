10bee85417a194887b382c09c18ae21fb4e104a6
Łukasz Gardecki <lukas.g1804@gmail.com>
2025-09-02 11:55:59 +0200

    Ćwiczenie wzorce - Command 1

---
+++ Wzorce_Projektowe/wzorce_behawioralne/command/command_1/src/main/java/Document.java
@@ -0,0 +1,10 @@
+public class Document {
+
+    public void save() {
+        System.out.println("Saving document...");
+    }
+
+    public void print() {
+        System.out.println("Printing document...");
+    }
+}
