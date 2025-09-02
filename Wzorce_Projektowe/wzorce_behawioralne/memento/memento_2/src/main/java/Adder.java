cf33096d8776937e74798d3d7bc837323b391960
Łukasz Gardecki <lukas.g1804@gmail.com>
2025-09-02 16:14:36 +0200

    Ćwiczenie wzorce - Memento 2

---
+++ Wzorce_Projektowe/wzorce_behawioralne/memento/memento_2/src/main/java/Adder.java
@@ -0,0 +1,20 @@
+public class Adder {
+  private int result;
+  private final Memento memento = new Memento(result);
+
+  public void add(int newNumber) {
+    result += newNumber;
+  }
+
+  public int getResult() {
+    return result;
+  }
+
+  public void save() {
+    memento.setState(result);
+  }
+
+  public void undo() {
+    result = memento.getState();
+  }
+}
