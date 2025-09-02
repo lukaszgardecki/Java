fbb00cc95730aa3d9f939e58563441ee452db4fe
Łukasz Gardecki <lukas.g1804@gmail.com>
2025-09-02 20:32:11 +0200

    Ćwiczenie wzorce - Visitor 2

---
+++ Wzorce_Projektowe/wzorce_behawioralne/visitor/visitor_2/src/main/java/SalaryAdjustmentVisitor.java
@@ -0,0 +1,17 @@
+public class SalaryAdjustmentVisitor implements Visitor {
+
+    @Override
+    public void visit(Manager manager) {
+        manager.setSalary(65000);
+    }
+
+    @Override
+    public void visit(SalesPerson salesPerson) {
+        salesPerson.setSalary(57000);
+    }
+
+    @Override
+    public void visit(StaffList staffList) {
+        System.out.println("Salary adjustments have been applied to all staff.");
+    }
+}
---
+++ Wzorce_Projektowe/wzorce_behawioralne/visitor/visitor_2/src/main/java/Visitor.java
@@ -0,0 +1,6 @@
+public interface Visitor {
+
+    void visit(Manager manager);
+    void visit(SalesPerson salesPerson);
+    void visit(StaffList staffList);
+}
