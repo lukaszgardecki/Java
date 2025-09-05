92b07fd3cbeec9f27208f33b39a110a0fab2fff7
Łukasz Gardecki <lukas.g1804@gmail.com>
2025-09-05 12:29:10 +0200

    Wzorce ćwiczenia - Prototype 1

---
+++ Wzorce_Projektowe/wzorce_kreacyjne/prototype/prototype_1/src/main/java/com/example/prototype/Meal.java
@@ -0,0 +1,20 @@
+package com.example.prototype;
+
+public abstract class Meal {
+
+    private int price;
+
+    public Meal(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public abstract Meal clone();
+}
