bd696c24aa2b5540b0d1dc11b06c853a8f91683c
Łukasz Gardecki <lukas.g1804@gmail.com>
2025-09-06 18:15:58 +0200

    Wzorce ćwiczenia - Flyweight 4

---
+++ Wzorce_Projektowe/wzorce_strukturalne/flyweight/flyweight_4/src/main/java/com/example/flyweight/FlowerFactory.java
@@ -0,0 +1,12 @@
+package com.example.flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlowerFactory {
+    private static final Map<String, Flower> flowerCache = new HashMap<>();
+
+    public static Flower createFlower(String name) {
+        return flowerCache.computeIfAbsent(name, Flower::new);
+    }
+}
