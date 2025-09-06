7d2aa716873d8552148871d7938c856b4e6d9e58
Łukasz Gardecki <lukas.g1804@gmail.com>
2025-09-06 18:10:18 +0200

    Wzorce ćwiczenia - Flyweight 3

---
+++ Wzorce_Projektowe/wzorce_strukturalne/flyweight/flyweight_3/src/main/java/com/example/flyweight/OrderFactory.java
@@ -0,0 +1,12 @@
+package com.example.flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OrderFactory {
+    private static final Map<String, Order> orderCache = new HashMap<>();
+
+    public static Order createOrder(String name) {
+        return orderCache.computeIfAbsent(name, Order::new);
+    }
+}
