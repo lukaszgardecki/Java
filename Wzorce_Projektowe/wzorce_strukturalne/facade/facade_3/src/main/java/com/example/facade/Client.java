ee5ffe3a38c4a08b6124c51e51f98c02035a7ffb
Łukasz Gardecki <lukas.g1804@gmail.com>
2025-09-06 17:54:26 +0200

    Wzorce ćwiczenia - Facade 3

---
+++ Wzorce_Projektowe/wzorce_strukturalne/facade/facade_3/src/main/java/com/example/facade/Client.java
@@ -0,0 +1,21 @@
+package com.example.facade;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class Client {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException {
+
+        String input = "Some text to encrypt";
+        EncryptionFacade encryptionFacade = new EncryptionFacade();
+        encryptionFacade.encrypt(input);
+    }
+
+}
